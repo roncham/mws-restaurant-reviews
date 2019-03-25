916dd03a97f90726093a8270228ce87bfc3b4785
Ron Chamberlain <roncham@users.noreply.github.com>
2019-03-25 10:42:51 -0700

    Update config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 let config = {
-  MY_KEY: 'pk.eyJ1Ijoicm9uY2hhbSIsImEiOiJjamliNGN1NGgxNXZxM3BzOTNlZjlleHkxIn0.ikVaXa0UsO1KA3Dr0Z0ddg'
-  //'ADD_MAPBOX_KEY_HERE'
+  MY_KEY: //'ADD_MAPBOX_KEY_HERE'
 };
